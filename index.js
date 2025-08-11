b26233960e98a20587ae327d62e5364d302b182c
elweboss <jared117118119120@gmail.com>
2025-08-10 22:40:42 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/alexa', require('./routes/alexaRoute'));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
